058ae2c5ff9b42dd8262239cec238f17c4d3f32f
Rene <102662801+ReneSambou@users.noreply.github.com>
2023-06-26 16:18:39 +0000

    Rene's second commit
    Deleted the view pager adapted because it is no longer needed

---
+++ app/src/main/java/com/example/recipe_guide/page2.java
@@ -0,0 +1,12 @@
+package com.example.recipe_guide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class page2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page2);
+    }
+}
